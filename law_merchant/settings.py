619e5519576cccc1c37f9c578d014de6f587ac14
Anuj9821 <walsepatilanuj9821@gmail.com>
2024-02-03 09:37:30 +0530

    new css updated

--- law_merchant/settings.py
+++ law_merchant/settings.py
@@ -131,3 +131,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
